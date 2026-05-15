3883eff31ffc58c395b51c11cd0ef6d5b51c80ed
wangchuan <976148791@qq.com>
2026-05-15 14:08:37 +0800

    日志修改

--- core/logger.py
+++ core/logger.py
@@ -1,10 +1,18 @@
 """日志模块"""
 
+import io
 import logging
 import os
+import sys
 from datetime import datetime
 
 from config.settings import LOG_DIR
+
+# 强制 stdout/stderr 使用 UTF-8，解决 Jenkins Windows 下中文乱码
+if hasattr(sys.stdout, 'buffer'):
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', line_buffering=True)
+if hasattr(sys.stderr, 'buffer'):
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8', line_buffering=True)
 
 
 def get_logger(name: str = "automation") -> logging.Logger:
